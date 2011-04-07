6b493f71695ad952b5228859f53f2887329a42ad
Kyle Wilcox <kwilcox@asascience.com>
2011-04-07 17:07:20 -0400

    Update the tests

--- src/com/asascience/edc/tests/LoadFromFile.java
+++ src/com/asascience/edc/tests/LoadFromFile.java
@@ -1,7 +1,6 @@
 package com.asascience.edc.tests;
 
 import java.io.IOException;
-import java.util.Formatter;
 import ucar.nc2.dataset.NetcdfDataset;
 import ucar.nc2.thredds.ThreddsDataFactory;
 import ucar.unidata.geoloc.LatLonRect;
@@ -15,17 +14,22 @@ public class LoadFromFile {
   }
 
   private static void testEDC() throws IOException {
-    ThreddsDataFactory.Result tdf = new ThreddsDataFactory().openFeatureDataset("http://edac-dap.northerngulfinstitute.org/thredds/catalog/ncom_reg6_agg/catalog.xml#ncom_reg6_agg/NCOM_Region_6_Aggregation_best.ncd",null);
+
+    String dap = "http://edac-dap3.northerngulfinstitute.org/thredds/dodsC/ncom_fukushima_agg/Fukushima_best.ncd";
+    String tdslink = "http://edac-dap3.northerngulfinstitute.org/thredds/ncom_fukushima_agg/catalog.xml#ncom_fukushima_agg/Fukushima_best.ncd";
+    
+    ThreddsDataFactory.Result tdf = new ThreddsDataFactory().openFeatureDataset(tdslink,null);
     ucar.nc2.dt.grid.GridDataset gds1 = (ucar.nc2.dt.grid.GridDataset)tdf.featureDataset;
     LatLonRect bounds1 = gds1.getBoundingBox();
     System.out.println(bounds1);
-
-    NetcdfDataset ncd = new ThreddsDataFactory().openDataset("http://edac-dap.northerngulfinstitute.org/thredds/catalog/ncom_reg6_agg/catalog.xml#ncom_reg6_agg/NCOM_Region_6_Aggregation_best.ncd",true,null,null);
+    
+    NetcdfDataset ncd = new ThreddsDataFactory().openDataset(tdslink,true,null,null);
     ucar.nc2.dt.grid.GridDataset gds2 = ucar.nc2.dt.grid.GridDataset.open(ncd.getLocation());
     LatLonRect bounds2 = gds2.getBoundingBox();
     System.out.println(bounds2);
-
-    ucar.nc2.dt.grid.GridDataset gds3 = ucar.nc2.dt.grid.GridDataset.open("http://edac-dap.northerngulfinstitute.org/thredds/dodsC/ncom_reg6_agg/NCOM_Region_6_Aggregation_best.ncd");
+    
+    
+    ucar.nc2.dt.grid.GridDataset gds3 = ucar.nc2.dt.grid.GridDataset.open(dap);
     LatLonRect bounds3 = gds3.getBoundingBox();
     System.out.println(bounds3);
   }
