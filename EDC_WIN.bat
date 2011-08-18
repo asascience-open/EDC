c0411759e3776dfc3c61ecaee4c1d8a223c9a268
Kyle Wilcox <kwilcox@asascience.com>
2011-08-18 16:06:10 -0400

    Update the libraries for different platforms

--- EDC_WIN.bat
+++ EDC_WIN.bat
@@ -1,4 +1,4 @@
 @echo off
 cd "%1"
 echo Running Java Environmental Data Connector...
-"java" -Djava.library.path=lib/wwj -Xmx512m -jar "EDC_Standalone.jar"
+"java" -Djava.library.path=lib/wwj -Dsun.java2d.noddraw=true -Xmx512m -jar "EDC_Standalone.jar"
